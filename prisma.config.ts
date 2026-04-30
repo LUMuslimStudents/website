8b5f2c72bf7f3cfbba5e4d686dbb20e6700213a9
abdulrahim-ss <abdulraheemssulaiman@outlook.com>
2026-04-30 19:42:37 +0200

    Fix for prisma. - Now we can switch backends and choose prisma schema depending on DB type

---
+++ prisma.config.ts
@@ -0,0 +1,29 @@
+import { defineConfig, env } from "prisma/config";
+import "dotenv/config";
+
+const BACKEND = env("VITE_BACKEND");
+
+let db_url: string;
+let schema: string;
+
+if (BACKEND === "REST") {
+    db_url = "MYSQL_URL";
+    schema = "schema.mysql.prisma"; 
+  }
+  else if (BACKEND === "supabase") {
+    db_url = "VITE_SUPABASE_URL";
+    schema = "schema.postgres.prisma"; 
+}
+else {
+    throw new Error("Need to specify valid backend in `.env`");
+}
+
+export default defineConfig({
+  schema: `backend/prisma/${schema}`,
+  migrations: {
+    path: "backend/prisma/migrations",
+  },
+  datasource: {
+    url: env(db_url),
+  },
+});
